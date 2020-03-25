f1ba36a0909f369249d79bd626b29828e9f7cf23
Jan Vainer <vainerjan@gmail.com>
2020-03-25 21:22:43 +0100

    finalize

--- generate_config.py
+++ generate_config.py
@@ -14,7 +14,7 @@ with open(os.path.join(folder, 'test_sentences.txt')) as f:
 
 output = {
     'testname': 'Compare TTS models',
-    'testId': 'tts_random',
+    'testId': 'tts_experiment',
     'bufferSize': '4096',  # this actually influences if the app fails or not :/
     'stopOnErrors': True,
     'showButtonPreviousPage': True,
@@ -24,8 +24,8 @@ output = {
         {
           'type': 'generic',
           'id': 'intro_page',
-          'name': 'Welcome to this survey',
-          'content': 'Some welcome text.',
+          'name': 'Speech synthesis evaluation',
+          'content': 'Hi, welcome to this speech synthesis survey. My name is Jan Vainer and I need your help! In my thesis, I tried to develop a high-quality speech synthesis system. But before submitting the thesis, the system needs a quality evaluation. Please evaluate the following sentence recordings according to perceived quality and speech fluency. On each page, you will be presented with 5 recordings of the same sentence. One recording is synthesized from my system, three recordings are synthesized from other systems and the last recording is a human speaker. The recordings are randomly shuffled. There will be 10 pages in total and the survey should take between 5 to 7 minutes. :)',
         }
     ]
 }
@@ -34,8 +34,8 @@ for i, s in enumerate(sentences):
     page = {
       'type': 'mushra',
       'id': 'sentence'+str(i),
-      'name': 'Some name',
-      'content': s,
+      'name': s,
+      'content': 'Please rate each audio according to perceived speech quality and fluency.',
       'showWaveform': True,
       'enableLooping': False,
       'reference': f'configs/resources/audio/synthesized_audio/reference/{i}.wav',
@@ -47,6 +47,7 @@ for i, s in enumerate(sentences):
           'deep_voice': f'configs/resources/audio/synthesized_audio/deepvoice3/{i}.wav',
           'efficient_tts': f'configs/resources/audio/synthesized_audio/efficient_tts/{i}.wav',
           'tacotron_2': f'configs/resources/audio/synthesized_audio/tacotron_2/{i}.wav',
+          'fertility': f'configs/resources/audio/synthesized_audio/fertility/{i}.wav',
         }    
     }
     output['pages'].append(page)
@@ -56,7 +57,7 @@ output['pages'].append(
     {
       'type': 'finish',
       'name': 'Thank you!',
-      'content': 'Some welcome text.',
+      'content': 'Your evaluation is much appreciated! ;)',
       'writeResults': True
     }
 )
